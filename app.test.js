2770c5a5834c9517b6dd134718ed64051e46539f
daniell.rotaru@gmail.com <daniell.rotaru@gmail.com>
2017-04-06 16:08:21 +0200

    Ch2-Templates

---
+++ app.test.js
@@ -0,0 +1,13 @@
+﻿describe("PhoneListController", function() {
+
+  beforeEach(module("phonecatApp"));
+
+  it("should create a 'phones' model with 3 phone", inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller("PhoneListController", {$scope: scope});
+
+    expect(scope.phones.length).toBe(3);
+    expect(scope.name).toBe("world");
+
+  }));
+})
